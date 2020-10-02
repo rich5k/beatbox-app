593ad967305f1c2fdde12dcd2a9d1fe09a2ed1e1
rich5k <richardanatsui@gmail.com>
2020-10-02 20:45:56 +0100

    added beat to first pad

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 //TODO: Connect key press play a beat
 const playBeat= (buttonKey)=>{
     if(buttonKey== 'a'){
-        let audio= new Audio()
+        let audio= new Audio('./assets/drum snare.mp3');
+        audio.play();
     }
 }
 document.addEventListener('keydown',()=>{
     console.log(event.key);
+    playBeat(event.key);
 })
 
