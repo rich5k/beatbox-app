740b0504fff386b8bae5141920a41596bdf95a0c
rich5k <richardanatsui@gmail.com>
2020-10-02 20:42:34 +0100

    added beats to assets folder

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+//TODO: Create a key listener
+//TODO: Connect key press play a beat
+const playBeat= (buttonKey)=>{
+    if(buttonKey== 'a'){
+        let audio= new Audio()
+    }
+}
 document.addEventListener('keydown',()=>{
     console.log(event.key);
 })
+
